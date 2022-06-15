0d1f777cd4d0cb897ad7fdba2d0a92b3ab299579
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-06-14 20:10:04 -0500

    Implementacion de react-router y estructuracion de carpetas ok

---
+++ src/JournalApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppRoutes } from './router/AppRoutes'
+
+
+export const JournalApp = () => {
+    return (
+        <>
+            <AppRoutes />
+        </>
+    )
+}
