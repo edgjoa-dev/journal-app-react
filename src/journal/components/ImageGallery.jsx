04c07118d558a3b5c3f2f1d61e3addb94231c1d4
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-06-25 23:59:04 -0500

    Galeria implementada ok

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -5,7 +5,23 @@ export const ImageGallery = () => {
 
 
     return (
-        <h2>Image Gallery</h2>
+        <ImageList sx={{ width: 500, height: 450 }}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={item.title}
+              subtitle={<span>by: {item.author}</span>}
+              position="below"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     )
 
 
