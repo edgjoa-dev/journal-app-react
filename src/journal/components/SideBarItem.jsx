678528e777f2459f21b6e5f69d53a1ccb1f088c1
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-07-04 09:50:40 -0500

    Mostrar notas desde sidebaritem

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,8 +1,15 @@
+import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal/journalSlice";
 import { TurnedInNot } from "@mui/icons-material"
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
-import { useMemo } from "react";
 
-export const SideBarItem = ({title = '', body, id}) => {
+export const SideBarItem = ({title = '', body, id, date, imageURL}) => {
+    const dispatch = useDispatch();
+
+    const onClickNote = () => {
+        dispatch(setActiveNote({id, body, title, date, imageURL}))
+    }
 
     const newTitle = useMemo( ()=> {
         return title.length > 17
@@ -17,7 +24,7 @@ export const SideBarItem = ({title = '', body, id}) => {
 
     return (
         <ListItem disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={onClickNote} >
                 <ListItemIcon>
                     <TurnedInNot />
                 </ListItemIcon>
